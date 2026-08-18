2216fb1fcb9c167612c58d4f78d3942d33577bd6
Mailisha Awuor <mailishaawuor@gmail.com>
2026-08-18 16:56:02 +0300

    refactor: rename app folder to rejesha_green and update fastapi imports

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from config import settings
+from rejesha_green.config import settings
 
 
 engine = create_engine(
--- rejesha_green/config.py
+++ rejesha_green/config.py
@@ -1,17 +1,11 @@
-from pathlib import Path
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 class Settings(BaseSettings):
-
     DATABASE_URL: str
 
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / ".env"
+        env_file=".env"
     )
 
 

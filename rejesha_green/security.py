1494120b28ad80627deaea0962ada9bbd48dc48b
Mailisha Awuor <mailishaawuor@gmail.com>
2026-08-26 15:36:14 +0300

    mkdofj

--- rejesha_green/security.py
+++ rejesha_green/security.py
@@ -6,8 +6,8 @@ from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
 from rejesha_green.config import settings
 
 bearer_scheme=HTTPBearer()
-PRIVATE_KEY=Path(settings.JWT_PRIVATE_KEY_PATH).read_text()
-PUBLIC_KEY=Path(settings.JWT_PUBLIC_KEY_PATH).read_text()
+PRIVATE_KEY=(settings.JWT_PRIVATE_KEY)
+PUBLIC_KEY=(settings.JWT_PUBLIC_KEY)
 
 def hash_password(password:str): return bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt()).decode("utf-8")
 

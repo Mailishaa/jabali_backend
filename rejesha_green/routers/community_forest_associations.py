e683613c651110722d6bb75e517cb42870f3bef3
Mailisha Awuor <mailishaawuor@gmail.com>
2026-09-06 11:44:05 +0300

    CFA services

--- rejesha_green/routers/community_forest_associations.py
+++ rejesha_green/routers/community_forest_associations.py
@@ -15,9 +15,20 @@ def create_cfa(data: CommunityForestAssociationCreate, db: Session = Depends(get
     return community_forest_association_service.create_community_forest_association(db, data, current_user)
 
 @router.get("/", response_model=list[CommunityForestAssociationResponse])
-def get_cfas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user=Depends(require_role(UserRole.SUPER_ADMIN,UserRole.KENYA_FOREST_SERVICE_OFFICIAL))): 
-    return CommunityForestAssociationRepository(db).get_all(skip, limit)
-
+def get_cfas(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+    current_user=Depends(
+        require_role(
+            UserRole.SUPER_ADMIN,
+            UserRole.KENYA_FOREST_SERVICE_OFFICIAL
+        )
+    )
+):
+    return community_forest_association_service.get_community_forest_associations(
+        db, skip, limit
+    )
 @router.get("/{cfa_id}", response_model=CommunityForestAssociationResponse)
 def get_cfa(cfa_id: uuid.UUID, db: Session = Depends(get_db), current_user=Depends(require_role(UserRole.SUPER_ADMIN,UserRole.KENYA_FOREST_SERVICE_OFFICIAL))):
     cfa = CommunityForestAssociationRepository(db).get(cfa_id)
